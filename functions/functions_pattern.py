302cd4f580abaa62f9ba90cabb0682745ff02595
vanammanohar <vanammanohar12@gmail.com>
2022-01-26 11:31:31 -0800

    Functions

---
+++ functions/functions_pattern.py
@@ -0,0 +1,12 @@
+def pattern(num=5):
+    for i in range(1,num+1):
+        print ('*' * i)
+
+pattern()
+# def print_pattern(num=5):
+#     for i in range(num):
+#         s = ''
+#         for j in range(i+1):
+#             s += "*"
+#         print(s)
+# print_pattern(6)
